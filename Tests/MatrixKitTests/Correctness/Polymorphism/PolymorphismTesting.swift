ded7560e3a52da60344299aa6f86ee1e680d2b6e
Sylvan Martin <alephnulltim@gmail.com>
2023-05-13 00:19:00 -0400

    Implemented ability to implement an arbitrary field and use matrix operations with entries in that field

--- Tests/MatrixKitTests/Correctness/Polymorphism/PolymorphismTesting.swift
+++ Tests/MatrixKitTests/Correctness/Polymorphism/PolymorphismTesting.swift
@@ -9,36 +9,49 @@ import XCTest
 import MatrixKit
 
 /**
- * The integers mod 2
+ * The integers mod 13
  */
-struct ZM2: FieldElement, ExpressibleByIntegerLiteral {
+struct ZM5: FieldElement, ExpressibleByIntegerLiteral {
     
     init(integerLiteral value: Int) {
-        self.value = Int(value.magnitude % 2)
+        self.value = Int(value.magnitude % 5)
     }
     
     typealias IntegerLiteralType = Int
     
-    static let zero: ZM2 = 0
-    static let one: ZM2 = 1
+    static let zero: ZM5 = 0
+    static let one: ZM5 = 1
     
     var value: Int
     
-    var inverse: ZM2 {
+    var inverse: ZM5 {
         assert(value != 0, "zero does not have an inverse")
-        return 1
+        // could do extgcd, but no
+        switch value {
+        case 1:
+            return 1
+        case 2:
+            return 3
+        case 3:
+            return 2
+        case 4:
+            return 4
+        default:
+            assert(value != 0, "zero does not have an inverse")
+            return -1
+        }
     }
     
-    static func * (lhs: ZM2, rhs: ZM2) -> ZM2 {
-        ZM2(integerLiteral: lhs.value * rhs.value)
+    static func * (lhs: ZM5, rhs: ZM5) -> ZM5 {
+        ZM5(integerLiteral: (lhs.value * rhs.value) % 5)
     }
     
-    static func + (lhs: ZM2, rhs: ZM2) -> ZM2 {
-        ZM2(integerLiteral: (lhs.value + rhs.value) % 2)
+    static func + (lhs: ZM5, rhs: ZM5) -> ZM5 {
+        ZM5(integerLiteral: (lhs.value + rhs.value) % 5)
     }
     
-    static prefix func - (rhs: ZM2) -> ZM2 {
-        rhs + 1
+    static prefix func - (rhs: ZM5) -> ZM5 {
+        ZM5(integerLiteral: 5 - rhs.value)
     }
     
     var description: String {
@@ -49,7 +62,7 @@ struct ZM2: FieldElement, ExpressibleByIntegerLiteral {
 
 final class PolymorphismTesting: XCTestCase {
     
-    typealias ZM = Matrix<ZM2>
+    typealias ZM = Matrix<ZM5>
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -65,8 +78,16 @@ final class PolymorphismTesting: XCTestCase {
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-        let iden = ZM.identity(forDim: 5)
-        print(iden + iden)
+        let rand = ZM(rows: 3, cols: 3) { _,_  in
+            ZM5(integerLiteral: Int.random(in: 0..<5))
+        }
+        
+        print(rand)
+        print(rand + rand)
+        
+        print(rand.reducedRowEchelonForm)
+        print(rand.inverse)
+        print(rand.inverse * rand)
     }
 
     func testPerformanceExample() throws {
